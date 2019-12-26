f5bdb07237ba1437e987c6da467baa238b30e435
Andrew Schetinin <andrey.schetinin@synesis-sport.by>
2019-12-26 13:00:44 +0300

    game: add build

---
+++ docs/precache-manifest.c7d541fc4a2a049066347ca39b8de165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546b9122f68de0d4ad40e4bac3e0069b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a15d725af6a0db7cae7",
+    "url": "/static/css/main.682604f5.chunk.css"
+  },
+  {
+    "revision": "0e01fbf2e19b4eebdb25",
+    "url": "/static/js/2.d513a994.chunk.js"
+  },
+  {
+    "revision": "7a15d725af6a0db7cae7",
+    "url": "/static/js/main.13d8101d.chunk.js"
+  },
+  {
+    "revision": "8b064deae2c66d092441",
+    "url": "/static/js/runtime-main.024ce66a.js"
+  }
+]);
